5602286563a4c7aae1131dbb04ac30d2c9c4a4cb
Nedko Georgiev <7398080+Sephiroth43@users.noreply.github.com>
2020-10-17 11:20:13 +0300

    optimizing Modal rerender

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 
 import Button from './../../UI/Backdrop/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li key={igKey}>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
-                </li>
-            );
-        });
+class OrderSummary extends Component {
+    // This could be a functional component, doesn't have to be a class
+    componentDidUpdate() {
+        console.log('[Order summary did update]')
+    }
+    
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+                    </li>
+                );
+            });
 
-    return (
-        <Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled} >CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued} >CONTINUE</Button>
-        </Fragment>
-    );
+        return (
+            <Fragment>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled} >CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued} >CONTINUE</Button>
+            </Fragment>
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -5,7 +5,9 @@ import classes from './DrawerToggle.module.css';
 const drawerToggle = (props) => {
     return (
         <div className={classes.DrawerToggle} onClick={props.clicked}>
-            |||
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     );
 };
