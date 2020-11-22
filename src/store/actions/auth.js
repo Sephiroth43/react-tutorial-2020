3b8b1f272d779798607e5230175c7b2c4311b958
Nedko Georgiev <7398080+Sephiroth43@users.noreply.github.com>
2020-11-22 17:44:58 +0200

    logging users out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,21 @@ export const authFail = (error) => {
     }
 }
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000)
+    }
+}
+
 export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
@@ -36,13 +51,14 @@ export const auth = (email, password, isSignup) => {
             url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBgNxwkCtbgU8SlpkCpIchUmuW_jNbw3vQ'
         }
         axios.post(url, authData)
-        .then(response => {
-            console.log(response);
-            dispatch(authSuccess(response.data.idToken, response.data.localId));
-        })
-        .catch(err => {
-            console.log(err);
-            dispatch(authFail(err.response.data.error));
-        });
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err.response.data.error));
+            });
     }
 }
