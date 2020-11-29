300cdc408d3a1ecf887f7ac5e9e31c0b5e2bf8e7
Nedko Georgiev <7398080+Sephiroth43@users.noreply.github.com>
2020-11-29 23:28:38 +0200

    moving orders side ffects into sagas

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,5 +1,4 @@
 import * as actionTypes from './actionTypes';
-import axios from '../../axios-orders';
 
 export const addIngredient = (name) => {
     return {
@@ -22,20 +21,14 @@ export const setIngredients = (ingredients) => {
     }
 }
 
-export const fetchINgredientsFailed = () => {
+export const fetchIngredientsFailed = () => {
     return {
         type: actionTypes.FETCH_INGREDIENTS_FAILED
     }
 }
 
 export const initIngredients = () => {
-    return dispatch => {
-        axios.get('/ingredients.json')
-            .then(response => {
-                dispatch(setIngredients(response.data))
-            })
-            .catch(error => {
-                dispatch(fetchINgredientsFailed());
-            });
-    };
+    return {
+        type: actionTypes.INIT_INGREDIENTS
+    }
 };
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,11 +1,22 @@
-import { generatePath } from 'react-router-dom'
 import { takeEvery } from 'redux-saga/effects'
 
 import * as actionTypes from '../actions/actionTypes'
-import { logoutSaga, checkAuthTimeoutSaga, authUserSaga } from './auth'
+import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth'
+import { initIngredientsSaga } from './burgerBuilder'
+import { purchaseBurgerSaga, fetchOrdersSaga } from './order'
 
 export function* watchAuth(params) {
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
     yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
+    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+}
+
+export function* watchBurgerBuilder() {
+    yield takeEvery(actionTypes.AUTH_CHECK_STATE, initIngredientsSaga);
+}
+
+export function* watchOrder() {
+    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+    yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
