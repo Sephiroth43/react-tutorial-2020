6fe7f81594ea9f777764dbf009892eff6d1f8275
Nedko Georgiev <7398080+Sephiroth43@users.noreply.github.com>
2020-11-29 22:15:07 +0200

    migrating moar to saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,8 +2,9 @@ import { generatePath } from 'react-router-dom'
 import { takeEvery } from 'redux-saga/effects'
 
 import * as actionTypes from '../actions/actionTypes'
-import { logoutSaga } from './auth'
+import { logoutSaga, checkAuthTimeoutSaga } from './auth'
 
 export function* watchAuth(params) {
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
 }
