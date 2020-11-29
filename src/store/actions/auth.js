f15aafbbf068202f962a435384103d9a7b5840e9
Nedko Georgiev <7398080+Sephiroth43@users.noreply.github.com>
2020-11-29 22:27:52 +0200

    handling authentication with a saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -54,31 +54,11 @@ export const setAuthRedirectPath = (path) => {
 
 
 export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        };
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBgNxwkCtbgU8SlpkCpIchUmuW_jNbw3vQ';
-        if (!isSignup) {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBgNxwkCtbgU8SlpkCpIchUmuW_jNbw3vQ'
-        }
-        axios.post(url, authData)
-            .then(response => {
-                // console.log(response);
-                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-                localStorage.setItem('token', response.data.idToken);
-                localStorage.setItem('expirationDate', expirationDate);
-                localStorage.setItem('userId', response.data.localId);
-                dispatch(authSuccess(response.data.idToken, response.data.localId));
-                dispatch(checkAuthTimeout(response.data.expiresIn));
-            })
-            .catch(err => {
-                // console.log(err);
-                dispatch(authFail(err.response.data.error));
-            });
+    return {
+        type: actionTypes.AUTH_USER,
+        email: email,
+        password: password,
+        isSignup: isSignup
     }
 }
 
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,9 +2,10 @@ import { generatePath } from 'react-router-dom'
 import { takeEvery } from 'redux-saga/effects'
 
 import * as actionTypes from '../actions/actionTypes'
-import { logoutSaga, checkAuthTimeoutSaga } from './auth'
+import { logoutSaga, checkAuthTimeoutSaga, authUserSaga } from './auth'
 
 export function* watchAuth(params) {
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
+    yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
 }
