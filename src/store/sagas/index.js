22cfbafddd54df8970ce10e352fe10c92575fee8
Nedko Georgiev <7398080+Sephiroth43@users.noreply.github.com>
2020-11-29 21:54:27 +0200

    moving logic from the action creator to a saga

---
+++ src/store/sagas/index.js
@@ -0,0 +1,9 @@
+import { generatePath } from 'react-router-dom'
+import { takeEvery } from 'redux-saga/effects'
+
+import * as actionTypes from '../actions/actionTypes'
+import { logoutSaga } from './auth'
+
+export function* watchAuth(params) {
+    yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+}
