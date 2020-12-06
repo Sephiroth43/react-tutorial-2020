b25c0dd000a3f5bfcff313ff71b208cc81473214
Nedko Georgiev <7398080+Sephiroth43@users.noreply.github.com>
2020-12-06 22:53:25 +0200

    using react Memo and More!

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,38 +1,36 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment } from 'react';
 
 import Button from './../../UI/Button/Button';
 
 // could be converted into functional component
-class OrderSummary extends Component {
+const OrderSummary = props => {
     // This could be a functional component, doesn't have to be a class
     // componentDidUpdate() {
-        // console.log('[Order summary did update]')
+    // console.log('[Order summary did update]')
     // }
 
-    render() {
-        const ingredientSummary = Object.keys(this.props.ingredients)
-            .map(igKey => {
-                return (
-                    <li key={igKey}>
-                        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
-                    </li>
-                );
-            });
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                </li>
+            );
+        });
 
-        return (
-            <Fragment>
-                <h3>Your Order</h3>
-                <p>A delicious burger with the following ingredients:</p>
-                <ul>
-                    {ingredientSummary}
-                </ul>
-                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
-                <p>Continue to Checkout?</p>
-                <Button btnType="Danger" clicked={this.props.purchaseCanceled} >CANCEL</Button>
-                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
-            </Fragment>
-        );
-    }
+    return (
+        <Fragment>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCanceled} >CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+        </Fragment>
+    );
 };
 
 export default OrderSummary;
